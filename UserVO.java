c8c9d3ac0cc38cc7801fa55df4b504c89f7a3be1
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-10-20 20:42:06 +0200

    Create UserVO.java

---
+++ UserVO.java
@@ -0,0 +1,61 @@
+package es.unizar.sisinf.grp1.model;
+
+import java.util.Date;
+
+/**
+ * tabla usuarioRegistrado
+ * @author sisinf
+ *
+ */
+public class UserVO {
+	
+	private String userName;
+	private String password;
+	private Date lastLogin;
+	private boolean esAdmin;
+	
+	/**
+	 * Constructor
+	 * @param userName
+	 * @param password
+	 */
+	public UserVO(String userName, String password, Date lastLogin, boolean esAdmin) {
+		this.userName = userName;
+		this.password = password;
+		this.lastLogin = lastLogin;
+		this.esAdmin = esAdmin;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getLastLogin() {
+		return lastLogin;
+	}
+
+	public void setLastLogin(Date lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+
+	public boolean isEsAdmin() {
+		return esAdmin;
+	}
+
+	public void setEsAdmin(boolean esAdmin) {
+		this.esAdmin = esAdmin;
+	}
+	
+}
