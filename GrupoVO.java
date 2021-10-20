47da18d6bae909880c1b4ed171372b608a5de6ad
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-10-20 20:26:45 +0200

    Create GrupoVO.java

---
+++ GrupoVO.java
@@ -0,0 +1,38 @@
+package es.unizar.sisinf.grp1.model;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * tabla users
+ * @author Ernesto Bielsa Gracia
+ *
+ */
+public class GrupoVO {
+	
+	private String nombre;
+	private Integer n_equipos;
+	/**
+	 * Constructor
+	 */
+	public GrupoVO(String nombre, Integer n_equipos) {
+		super();
+		this.nombre = nombre;
+		this.n_equipos = n_equipos;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getN_equipos() {
+		return n_equipos;
+	}
+	public void setN_equipos(Integer n_equipos) {
+		this.n_equipos = n_equipos;
+	}
+	
+
+	
+}
