fb35d0099d20bef074693509951c5dcbcdb8804b
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2021-10-20 20:52:04 +0200

    Update EquipoFacade.java

--- EquipoFacade.java
+++ EquipoFacade.java
@@ -8,5 +8,28 @@ import java.sql.SQLException;
 import es.unizar.sisinf.grp1.db.PoolConnectionManager;
 
 public class equipoFacade {
+	private static String countByUserName = "SELECT count(*) cuenta FROM users WHERE username = ?";
+	private static String findByUserName = "SELECT * FROM users WHERE username = ?";
+	private static String updateDate = "UPDATE users set last_login = current_timestamp where username = ?";
+	
+	public String getTeam(String teamName) {
+		Connection conn = null;
+		equipoVO equipo = null;
 
+		try {
+			// Abrimos la conexiÃ³n e inicializamos los parÃ¡metros 
+			conn = PoolConnectionManager.getConnection(); 
+			PreparedStatement ps = conn.prepareStatement("Select * from equipos where nombre= ?");
+			ps.setString(1, teamName);
+			ResultSet rset = ps.executeQuery();
+			rset.next();
+			equipo = new equipoVO(rset.getString("teamName"),rset.getString("stadium"),rset.getInteger("precio"),
+								rset.getString("stadium"),rset.getString("grupo"),rset.getString("stadium"),rset.getString("stadium"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			PoolConnectionManager.releaseConnection(conn);
+		}
+		return equipo;
+	}
 }
