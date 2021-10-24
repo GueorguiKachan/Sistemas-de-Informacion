bd8b7b8a52b9ec60908f328f6a38088fe77cf4a2
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2021-10-24 19:15:17 +0200

    Update EquipoFacade.java

--- EquipoFacade.java
+++ EquipoFacade.java
@@ -19,12 +19,15 @@ public class equipoFacade {
 		try {
 			// Abrimos la conexiÃ³n e inicializamos los parÃ¡metros 
 			conn = PoolConnectionManager.getConnection(); 
+			
 			PreparedStatement ps = conn.prepareStatement("Select * from equipos where nombre= ?");
-			ps.setString(1, teamName);
+			ps.setString(1, teamName); // setString asigna el valor del 2º argumento al parámetro que está en la posición del 1º argumento. Sustituye los ? 
 			ResultSet rset = ps.executeQuery();
-			rset.next();
-			equipo = new equipoVO(rset.getString("teamName"),rset.getString("stadium"),rset.getInteger("precio"),
-								rset.getString("stadium"),rset.getString("grupo"),rset.getString("stadium"),rset.getString("stadium"));
+			rset.next(); // Next mueve el cursor una fila adelante de su posición actual
+			
+			//getString-getInteger devuelven el valor que haya en la columna indicada o bien por un número o bien por su nombre
+			equipo = new equipoVO(rset.getString("nombre"),rset.getString("estadio"),rset.getInteger("precioEnt"),
+								rset.getInteger("numJugadores"),rset.getString("grupo"),rset.getInteger("capacidad"),rset.getString("tipoCesped"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
