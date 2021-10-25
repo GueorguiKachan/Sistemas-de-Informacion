9c718693878c6e29b42670f538a7069ccc0d19bb
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-10-25 08:16:02 +0200

    Update GrupoFacade.java

--- java/GrupoFacade.java
+++ java/GrupoFacade.java
@@ -1,1 +1,35 @@
+package es.unizar.sisinf.grp1.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import es.unizar.sisinf.grp1.db.PoolConnectionManager;
+
+public class GrupoFacade {
+	private static String countByUserName = "SELECT count(*) cuenta FROM users WHERE username = ?";
+	
+	public GrupoVO getGrupo(String groupName) {
+		Connection conn = null;
+		GrupoVO grupo = null;
+
+		try {
+			// Abrimos la conexiÃ³n e inicializamos los parÃ¡metros 
+			conn = PoolConnectionManager.getConnection(); 
+			
+			PreparedStatement ps = conn.prepareStatement("Select * from grupos where nombre= ?");
+			ps.setString(1, groupName); // setString asigna el valor del 2º argumento al parámetro que está en la posición del 1º argumento. Sustituye los ? 
+			ResultSet rset = ps.executeQuery();
+			rset.next(); // Next mueve el cursor una fila adelante de su posición actual
+			
+			//getString-getInteger devuelven el valor que haya en la columna indicada o bien por un número o bien por su nombre
+			grupo = new GrupoVO(rset.getString("nombre"),rset.getInt("n_equipoos"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			PoolConnectionManager.releaseConnection(conn);
+		}
+		return grupo;
+	}
+}
