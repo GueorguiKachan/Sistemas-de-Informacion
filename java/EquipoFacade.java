ab429001e9cde83ec68ed69028926ecf522959aa
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2021-10-30 17:43:43 +0200

    VO y Facade de equipo y jugador

--- java/EquipoFacade.java
+++ java/EquipoFacade.java
@@ -4,8 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import es.unizar.sisinf.grp1.db.PoolConnectionManager;
+import es.unizar.sisinf.grp1.db.ConnectionManager;
 
 public class EquipoFacade {
 	private static String countByUserName = "SELECT count(*) cuenta FROM users WHERE username = ?";
@@ -16,7 +18,7 @@ public class EquipoFacade {
 
 		try {
 			// Abrimos la conexiÃ³n e inicializamos los parÃ¡metros 
-			conn = PoolConnectionManager.getConnection(); 
+			conn = ConnectionManager.getConnection(); 
 			
 			PreparedStatement ps = conn.prepareStatement("Select * from equipos where nombre= ?");
 			ps.setString(1, teamName); // setString asigna el valor del 2º argumento al parámetro que está en la posición del 1º argumento. Sustituye los ? 
@@ -29,8 +31,37 @@ public class EquipoFacade {
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			PoolConnectionManager.releaseConnection(conn);
+			//PoolConnectionManager.releaseConnection(conn);
 		}
 		return equipo;
+	}
+	
+	public List<EquipoVO> todosGrupo(String groupName) {
+		Connection conn = null;
+		EquipoVO equipo = null;
+		List<EquipoVO> grupo = new ArrayList<>();
+
+		try {
+			// Abrimos la conexion e inicializamos los parametros 
+			conn = ConnectionManager.getConnection(); 
+			
+			PreparedStatement ps = conn.prepareStatement("Select * from equipos where grupo= ?");
+			ps.setString(1, groupName); // setString asigna el valor del 2º argumento al parámetro que está en la posición del 1º argumento. Sustituye los ? 
+			ResultSet rset = ps.executeQuery();
+			
+			while(rset.next()) { // Next mueve el cursor una fila adelante de su posición actual
+				equipo = new EquipoVO(rset.getString("nombre"),rset.getString("estadio"),rset.getInt("precioEnt"),
+						rset.getInt("numJugadores"),rset.getString("grupo"),rset.getInt("capacidad"),rset.getString("tipoCesped"));
+				grupo.add(equipo);
+			}
+			//getString-getInteger devuelven el valor que haya en la columna indicada o bien por un número o bien por su nombre
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//PoolConnectionManager.releaseConnection(conn);
+		}
+		return grupo;
+
 	}
 }
--- java/JugadorFacade.java
+++ java/JugadorFacade.java
@@ -4,22 +4,26 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import es.unizar.sisinf.grp1.db.PoolConnectionManager;
+import es.unizar.sisinf.grp1.db.ConnectionManager;
 
 public class JugadorFacade {
 	private static String countByUserName = "SELECT count(*) cuenta FROM users WHERE username = ?";
 	
-	public JugadorVO getPlayer(Integer id) {
+	public JugadorVO getPlayer(String name) {
 		Connection conn = null;
 		JugadorVO jugador = null;
-
+		System.out.println("Se llega al dao");
 		try {
 			// Abrimos la conexiÃ³n e inicializamos los parÃ¡metros 
-			conn = PoolConnectionManager.getConnection(); 
-			
+			conn = ConnectionManager.getConnection(); 
+			System.out.println("Se obtiene la conexion");
 			PreparedStatement ps = conn.prepareStatement("Select * from jugadores where nombre= ?");
-			ps.setString(1, id.toString()); // setString asigna el valor del 2º argumento al parámetro que está en la posición del 1º argumento. Sustituye los ? 
+			System.out.println("Antes de hacer print a la query");
+			ps.setString(1, name); // setString asigna el valor del 2º argumento al parámetro que está en la posición del 1º argumento. Sustituye los ? 
+			System.out.println("La query es "+ ps);
 			ResultSet rset = ps.executeQuery();
 			rset.next(); // Next mueve el cursor una fila adelante de su posición actual
 			
@@ -29,8 +33,38 @@ public class JugadorFacade {
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			PoolConnectionManager.releaseConnection(conn);
+			//PoolConnectionManager.releaseConnection(conn);
 		}
 		return jugador;
 	}
+	
+	public List<JugadorVO> mismoEquipo(String team) {
+		Connection conn = null;
+		List<JugadorVO> jugadores = null;
+		JugadorVO jugador = null;
+		System.out.println("Se llega al dao");
+		try {
+			// Abrimos la conexiÃ³n e inicializamos los parÃ¡metros 
+			conn = ConnectionManager.getConnection(); 
+			System.out.println("Se obtiene la conexion");
+			PreparedStatement ps = conn.prepareStatement("Select * from jugadores where equipo= ?");
+			System.out.println("Antes de hacer print a la query");
+			ps.setString(1, team); // setString asigna el valor del 2º argumento al parámetro que está en la posición del 1º argumento. Sustituye los ? 
+			System.out.println("La query es "+ ps);
+			ResultSet rset = ps.executeQuery();
+			while(rset.next()) { // Next mueve el cursor una fila adelante de su posición actual
+				jugador = new JugadorVO(rset.getInt("id"),rset.getInt("pJugados"),rset.getInt("pTitular"),rset.getString("nombre"),rset.getInt("goles"),
+						rset.getInt("nacido"),rset.getInt("rojas"),rset.getInt("amarillas"),rset.getString("equipo"));
+				jugadores.add(jugador);
+			}
+			//getString-getInteger devuelven el valor que haya en la columna indicada o bien por un número o bien por su nombre
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//PoolConnectionManager.releaseConnection(conn);
+		}
+		return jugadores;
+	}
+
 }
