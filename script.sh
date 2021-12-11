ff5eca681dd32420357d8c92541098c756c229fd
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-12-11 17:32:33 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Paramos y borramos la versión anterior del portal, si existe
-docker stop sisinf-database
-docker rm sisinf-database
+sudo docker stop sisinf-database
+sudo docker rm sisinf-database
 
 cd postgres
 unzip main.zip
 
 sudo docker build -t sisinf/postgresql:latest .
 
-sudo docker run --name sisinf-database -e ALLOW_EMPTY_PASSWORD=yes -d
-sisinf/postgresql:latest
+sudo docker run --name sisinf-database -e ALLOW_EMPTY_PASSWORD=yes -p 5432:5432 -d sisinf/postgresql:latest
+
 
 cd ..
 
 # Paramos y borramos la versión anterior del portal, si existe
-docker stop sisinf-tomcat
-docker rm sisinf-tomcat
+sudo docker stop sisinf-tomcat
+sudo docker rm sisinf-tomcat
 
 cd tomcat
 sudo docker build -t sisinf/tomcat:latest .
 
-docker run -d --name sisinf-tomcat \
+sudo docker run -d --name sisinf-tomcat \
   --link sisinf-database \
   -p 8080:8080 \
   sisinf/tomcat:latest
